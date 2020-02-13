470b68ffa2dfc680df41274bc197f63221f34e05
VladimirKozachenko <vladimir.kozachenko@catenamedia.com>
2020-02-13 16:34:05 +0200

    final application

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app-header.css';
 
-const AppHeader = ({toDo, done}) => {
-  return (
-    <div className="app-header d-flex">
-      <h1>Todo List</h1>
-      <h2>{toDo} more to do, {done} done</h2>
-    </div>
-  );
-};
+export default class AppHeader extends Component  {
 
-export default AppHeader;
+    render(){
+    const { toDo, done} = this.props;
+    return (
+        <div className="app-header d-flex">
+            <h1>Todo List</h1>
+            <h2>{toDo} more to do, {done} done</h2>
+        </div>
+    );
+}
+
+};
